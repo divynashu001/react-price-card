0548fddffd6774c45524d78771c7984a840c9a4e
divynashu001 <divyanshudhiman41@gmail.com>
2024-11-25 23:43:29 +0530

    Adding React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Price from "./components/Price"; 
+import "./index.css"
+
+   function App() {
+       return (
+           <>
+               <Price/>
+           </>
+       )
+   }
+
+   export default App;
